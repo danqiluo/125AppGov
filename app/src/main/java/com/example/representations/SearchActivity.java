ea6620710acc50d87c48a02f3bff541921414053
danqiluo <aliceuiuc@gmail.com>
2019-04-25 13:40:40 -0500

    add API key But still working on making API request

---
+++ app/src/main/java/com/example/representations/SearchActivity.java
@@ -0,0 +1,35 @@
+package com.example.representations;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+//import com.android.volley.RequestQueue;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_repre);
+        configureNextButton();
+        
+    }
+    private void configureNextButton() {
+        Button next = (Button) findViewById(R.id.Get_Location);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(SearchActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+}
+
